243344efbc2f2e2f660fb656fc04b5bfa32df9ed
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-05-26 01:04:18 -0300

    add novas funcionalidades

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom';
 import style from './Header.module.css';
+import { useState } from 'react';
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toogleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
       <header className={style.header}>
@@ -10,12 +17,17 @@ const Header = () => {
             andersonChaves.dev
           </Link>
         </div>
-        <nav>
+        <nav className={`${style.menuSandwich} ${showMenu ? style.show : ''}`} onClick={toogleMenu}>
           <Link to='/'>Home</Link>
           <Link to='/sobre'>Sobre</Link>
           <Link to='/projetos'>Projetos</Link>
           <Link to='/contatos'>Contatos</Link>
         </nav>
+        <div className={style.menuButton} onClick={toogleMenu}>
+          <span className={style.linha}></span>
+          <span className={style.linha}></span>
+          <span className={style.linha}></span>
+        </div>
       </header>
     </>
   );
--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,14 +2,14 @@ import style from './Page404.module.css';
 
 const Page404 = () => {
   return (
-    <>
+    <section className={style.page404}>
       <h2 className={style.titulo2}>Algo deu errado!</h2>
       <div className={style.textos}>
         <span className={style.texto_grande}>404</span>
         <br />
         <strong className={style.texto_vermelho}>Página não Localizada!</strong>
       </div>
-    </>
+    </section>
   );
 }
 
